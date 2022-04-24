5a24649124e51a8c4f0f62abdd76795dde35a9c9
andre-w <cibisova267@gmail.com>
2022-04-24 17:17:42 +0500

    Удаление и сохранение рисунка canvas

--- js/script.js
+++ js/script.js
@@ -1,30 +1,59 @@
-let draw = false;
+let draw = false
+let mouse = {
+   x: 0,
+   y: 0,
+}
 
 const canvas = document.querySelector('canvas')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
 const cx = canvas.getContext('2d')
+cx.canvas.width = window.innerWidth / 2
+cx.canvas.height = window.innerHeight
 
-const start = e => {
-    draw = true;
+const start = (e) => {
+   mouse.x = e.pageX - canvas.getBoundingClientRect().left
+   mouse.y = e.pageY - canvas.getBoundingClientRect().top
 
-    cx.beginPath();
-    cx.moveTo(e.pageX, e.pageY);
+   draw = true
+
+   cx.beginPath()
+   cx.moveTo(mouse.x, mouse.y)
 }
-const painting = e => {
-    if (draw == true) {
-        cx.lineTo(e.pageX, e.pageY);
-        cx.stroke();
-    }
+const painting = (e) => {
+   if (draw == true) {
+      mouse.x = e.pageX - canvas.getBoundingClientRect().left
+      mouse.y = e.pageY - canvas.getBoundingClientRect().top
+
+      cx.strokeStyle = '#000'
+      cx.lineTo(mouse.x, mouse.y)
+      cx.stroke()
+   }
 }
-const stop = e => {
-    cx.moveTo(e.pageX, e.pageY);
-    cx.stroke();
-    cx.closePath();
-    draw = false;
+const stop = (e) => {
+   mouse.x = e.pageX - canvas.getBoundingClientRect().left
+   mouse.y = e.pageY - canvas.getBoundingClientRect().top
+
+   cx.lineTo(mouse.x, mouse.y)
+   cx.stroke()
+   cx.closePath()
+   draw = false
 }
 
 canvas.addEventListener('mousedown', start)
-canvas.addEventListener("mousemove", painting)
-canvas.addEventListener("mouseup", stop)
+canvas.addEventListener('mousemove', painting)
+canvas.addEventListener('mouseup', stop)
+
+const save = document.querySelector('#save')
+
+save.addEventListener('click', () => {
+   const data = canvas.toDataURL('image/png')
+   save.href = data.replace(
+      /^data:image\/[^;]/,
+      'data:application/octet-stream'
+   )
+})
+
+const del = document.querySelector('#del')
+
+del.addEventListener('click', () => {
+   cx.clearRect(0, 0, canvas.width, canvas.height)
+})
